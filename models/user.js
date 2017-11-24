38b932dc41a7496ae49e4634e77f26a60c6e6f92
Jarrod Blanton <jarrodblanton13@gmail.com>
2017-11-24 15:44:00 -0500

    Added comments

--- models/user.js
+++ models/user.js
@@ -1,11 +1,13 @@
 var mongoose = require('mongoose');
 var passportLocalMongoose = require('passport-local-mongoose');
 
+// Create new schema for our user model
 var UserSchema = new mongoose.Schema({
     username: String,
     password: String 
 });
 
+// Add extra methods to the User model 
 UserSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", UserSchema);
