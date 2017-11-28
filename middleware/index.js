66afccf6057eef91be7e5ef6876fa8098a5eaedd
Jarrod Blanton <jarrodblanton13@gmail.com>
2017-11-28 15:48:03 -0500

    Added a bunch of error handling

--- middleware/index.js
+++ middleware/index.js
@@ -7,6 +7,7 @@ middleware.isLoggedIn = function(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
     }
+    req.flash('error', 'You must be logged in to do that');
     res.redirect('/login');
 }
 
@@ -14,18 +15,22 @@ middleware.isLoggedIn = function(req, res, next) {
 middleware.checkCampgroundOwnership = function(req, res, next) {
     if (req.isAuthenticated()) {
         Campground.findById(req.params.id, function(err, campground) {
-            if (err) {
-                res.redirect('back');
-            } else {
-                if (campground.author.id.equals(req.user._id)) {
-                    next();
-                } else {
-                    res.redirect('back');
-                }
+            // Some crazy shit to catch if a user tries to change the string of the ID to the exact same length as the original campground id
+            if (err || !campground) {
+                req.flash('error', 'Sorry! That campground does not exist!');
+                res.redirect('/campgrounds');
+            } else if (campground.author.id.equals(req.user._id) || req.user.isAdmin) {
+                req.campground = campground;
+                next();
+            } 
+            else {
+                req.flash('error', 'You don\'t have permission to do that!' );
+                res.redirect('/campgrounds/' + req.params.id);
             }
         });
     } else {// Sends user to previous route
-        res.redirect('back');
+        req.flash('error', 'You must be signed to edit your campground');
+        res.redirect('/campgrounds/' + req.params.id);
     }
 }
 
@@ -34,20 +39,24 @@ middleware.checkCommentOwnership = function(req, res, next) {
     // Check if user is logged in
     if (req.isAuthenticated()) {
         Comment.findById(req.params.comment_id, function(err, comment) {
-            if (err) {
-                res.redirect('back');
+            // Same thing here
+            if (err || !comment) {
+                req.flash('error', 'Sorry! That comment does not exist!');
+                res.redirect('/campgrounds');
+            } else if (comment.author.id.equals(req.user._id) || req.user.isAdmin) { // check if comment's auther is the current user
+                req.comment = comment;    
+                next();
             } else {
-                if (comment.author.id.equals(req.user._id)) { // check if comment's auther is the current user
-                    next();
-                } else {
-                    // user does not own comment
-                    res.redirect('back');
-                }
+                // user does not own comment
+                req.flash('error', 'You do not have permission to do that');
+                res.redirect('/campgrounds/' + req.params.id);
             }
+            
         });
     } else {
         // user is not signed in
-        res.redirect('back');
+        req.flash('error', 'You must be signed in to do that');
+        res.redirect('/camgprounds/' + req.params.id);
     }
 }
 
